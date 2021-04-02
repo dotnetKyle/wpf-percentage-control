459bd1a2b0c4682ba78649c739b39a6764982ee2
Kyle <dotnetKyle@gmail.com>
2021-04-02 15:51:18 -0400

    Added WpfDesignerHelper package

--- Percentage/ViewModel.cs
+++ Percentage/ViewModel.cs
@@ -8,19 +8,24 @@ namespace Percentage
     {
         public ViewModel() 
         {
-            Stocks = new ObservableCollection<Stock>
+            Stocks = new ObservableCollection<Stock>();
+            
+            if(WpfDesignerHelper.Designer.Active)
             {
-                // Stocks chosen at random
-                new Stock("KLR+", "Kaleyra, Inc.", .0),
-                new Stock("LNTH", "Lantheus Holdings, Inc.", .0),
-                new Stock("RNLC", "Large Cap US Equity Select", .0),
-                new Stock("STM", "STMicroelectronics N.V.", .0),
-                new Stock("CRMT", "America's Car-Mart, Inc.", .0),
-                new Stock("VIHAW", "VPC Impact Acquisition Holdings", .0),
-                new Stock("PHAR", "Pharming Group N.V. - ADS", .0),
-                new Stock("RDWR ", "Radware Ltd.", .0),
-                new Stock("HURC", "Hurco Companies, Inc.", .0)
-            };
+                Stocks = new ObservableCollection<Stock>
+                {
+                    // Stocks chosen at random
+                    new Stock("KLR+", "Kaleyra, Inc.", .0),
+                    new Stock("LNTH", "Lantheus Holdings, Inc.", .0),
+                    new Stock("RNLC", "Large Cap US Equity Select", .0),
+                    new Stock("STM", "STMicroelectronics N.V.", .0),
+                    new Stock("CRMT", "America's Car-Mart, Inc.", .0),
+                    new Stock("VIHAW", "VPC Impact Acquisition Holdings", .0),
+                    new Stock("PHAR", "Pharming Group N.V. - ADS", .0),
+                    new Stock("RDWR ", "Radware Ltd.", .0),
+                    new Stock("HURC", "Hurco Companies, Inc.", .0)
+                };
+            }
         }
 
         public ObservableCollection<Stock> Stocks { get; set; }
